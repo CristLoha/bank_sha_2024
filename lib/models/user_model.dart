ba1ab07e51bad5f6f3f7ef9813aef5fddd401e94
Christ Henry Loha <cristloha2205@gmail.com>
2024-02-07 18:13:31 +0800

    update

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -11,6 +11,7 @@ class UserModel {
   final String? cardNumber;
   final String? pin;
   final String? token;
+  final String? password;
   UserModel({
     this.id,
     this.name,
@@ -22,6 +23,7 @@ class UserModel {
     this.cardNumber,
     this.pin,
     this.token,
+    this.password,
   });
 
   Map<String, dynamic> toMap() {
@@ -36,26 +38,49 @@ class UserModel {
       'card_number': cardNumber,
       'pin': pin,
       'token': token,
+      'password': password,
     };
   }
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
-      id: map['id'],
-      name: map['name'],
-      email: map['email'],
-      username: map['username'],
-      verified: map['verified'],
-      profilePicture: map['profile_picture'],
-      balance: map['balance'],
-      cardNumber: map['card_number'],
-      pin: map['pin'],
-      token: map['token'],
-    );
+        id: map['id'],
+        name: map['name'],
+        email: map['email'],
+        username: map['username'],
+        verified: map['verified'],
+        profilePicture: map['profile_picture'],
+        balance: map['balance'],
+        cardNumber: map['card_number'],
+        pin: map['pin'],
+        token: map['token'],
+        password: map['password']);
   }
 
   String toJson() => json.encode(toMap());
 
   factory UserModel.fromJson(String source) =>
       UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  UserModel copyWith({
+    String? username,
+    String? name,
+    String? email,
+    String? pin,
+    String? password,
+    int? balance,
+  }) =>
+      UserModel(
+        id: id,
+        username: username ?? this.username,
+        name: name ?? this.name,
+        email: email ?? this.email,
+        pin: pin ?? this.pin,
+        password: password ?? this.password,
+        balance: balance ?? this.balance,
+        verified: verified,
+        profilePicture: profilePicture,
+        cardNumber: cardNumber,
+        token: token,
+      );
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
-
+import 'package:bank_sha/models/sign_up_form_model.dart';
 import 'package:bank_sha/shared/shared_values.dart';
 import 'package:http/http.dart' as http;
+
+import '../models/user_model.dart';
 
 class AuthService {
   Future<bool> checkEmail(String email) async {
@@ -20,6 +22,25 @@ class AuthService {
       }
     } catch (e) {
       print(e);
+      rethrow;
+    }
+  }
+
+  Future<UserModel> register(SignUpFormModel data) async {
+    try {
+      final res = await http.post(
+        Uri.parse('$baseUrl/api/register'),
+        body: data.toJson(),
+      );
+      if (res.statusCode == 200) {
+        UserModel user = UserModel.fromJson(res.body);
+        user = user.copyWith(password: data.password);
+
+        return user;
+      } else {
+        throw jsonDecode(res.body)['message'];
+      }
+    } catch (e) {
       rethrow;
     }
   }
