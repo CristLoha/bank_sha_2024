3d6e64ebd5a3556731cefd32755c9f0dd7c49c4a
Christ Henry Loha <cristloha2205@gmail.com>
2024-02-22 17:23:15 +0800

    update

--- lib/ui/pages/transfer/transfer_page.dart
+++ lib/ui/pages/transfer/transfer_page.dart
@@ -2,24 +2,24 @@ import 'package:bank_sha/blocs/user/user_bloc.dart';
 import 'package:bank_sha/models/transfer_form_model.dart';
 import 'package:bank_sha/models/user_model.dart';
 import 'package:bank_sha/shared/box_extension.dart';
-import 'package:bank_sha/shared/theme.dart';
-import 'package:bank_sha/ui/widgets/transfer_recent_user_item.dart';
-import 'package:bank_sha/ui/widgets/transfer_result_user_item.dart';
+import 'package:bank_sha/ui/pages/transfer/transfer_ammount_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../shared/theme.dart';
 import '../../widgets/custom_filled_button.dart';
 import '../../widgets/custom_form_field.dart';
-import 'transfer_ammount_page.dart';
+import '../../widgets/transfer_recent_user_item.dart';
+import '../../widgets/transfer_result_user_item.dart';
 
 class TransferPage extends StatefulWidget {
-  const TransferPage({Key? key}) : super(key: key);
+  const TransferPage({super.key});
 
   @override
   State<TransferPage> createState() => _TransferPageState();
 }
 
 class _TransferPageState extends State<TransferPage> {
-  final usernameController = TextEditingController(text: '');
+  final usernameC = TextEditingController(text: '');
   UserModel? selectedUser;
 
   late UserBloc userBloc;
@@ -43,9 +43,7 @@ class _TransferPageState extends State<TransferPage> {
           horizontal: 24,
         ),
         children: [
-          const SizedBox(
-            height: 30,
-          ),
+          40.heightBox,
           Text(
             'Search',
             style: blackTextStyle.copyWith(
@@ -53,28 +51,21 @@ class _TransferPageState extends State<TransferPage> {
               fontWeight: semiBold,
             ),
           ),
-          const SizedBox(
-            height: 14,
-          ),
+          14.heightBox,
           CustomFormField(
+            controller: usernameC,
             title: 'by username',
             isShowTitle: false,
-            controller: usernameController,
             onFieldSubmitted: (value) {
               if (value.isNotEmpty) {
-                userBloc.add(UserGetByUsername(usernameController.text));
+                userBloc.add(UserGetByUsername(usernameC.text));
               } else {
                 userBloc.add(UserGetRecent());
               }
               setState(() {});
             },
           ),
-          // buildRecentUsers(),
-          usernameController.text.isEmpty ? buildRecentUsers() : buildResult(),
-
-          const SizedBox(
-            height: 50,
-          ),
+          usernameC.text.isEmpty ? buildRecentUsers() : buildResult(),
         ],
       ),
       floatingActionButton: selectedUser != null
@@ -98,58 +89,6 @@ class _TransferPageState extends State<TransferPage> {
             )
           : Container(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
-
-  Widget buildRecentUsers() {
-    return Container(
-      margin: const EdgeInsets.only(
-        top: 40,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Recent Users',
-            style: blackTextStyle.copyWith(
-              fontSize: 16,
-              fontWeight: semiBold,
-            ),
-          ),
-          const SizedBox(
-            height: 14,
-          ),
-          BlocBuilder<UserBloc, UserState>(
-            builder: (context, state) {
-              if (state is UserSuccess) {
-                return Column(
-                  children: state.users.map((user) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => TransferAmountPage(
-                              data: TransferFormModel(
-                                sendTo: user.username,
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                      child: TransferRecentUserItem(user: user),
-                    );
-                  }).toList(),
-                );
-              }
-
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            },
-          ),
-        ],
-      ),
     );
   }
 
@@ -189,6 +128,55 @@ class _TransferPageState extends State<TransferPage> {
                         user: user,
                         isSelected: user.id == selectedUser?.id,
                       ),
+                    );
+                  }).toList(),
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildRecentUsers() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recent Users',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          14.heightBox,
+          BlocBuilder<UserBloc, UserState>(
+            builder: (context, state) {
+              if (state is UserSuccess) {
+                return Column(
+                  children: state.users.map((user) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TransferAmountPage(
+                              data: TransferFormModel(
+                                sendTo: user.username,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      child: TransferRecentUserItem(user: user),
                     );
                   }).toList(),
                 );
